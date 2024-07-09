92f2bf57eacc5cfb34e2de068fa83fcc6cfd7cea
Sina Zinsaz <101419921+SINAZZzz@users.noreply.github.com>
2024-07-09 23:53:47 +0330

    feat: add route in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
