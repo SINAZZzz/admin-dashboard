28d62e1b51b078c11dc62dff182c836a133432bb
Sina Zinsaz <101419921+SINAZZzz@users.noreply.github.com>
2024-07-10 01:16:53 +0330

    feat: add constants icons

---
+++ src/constants/icons.ts
@@ -0,0 +1,8 @@
+import cartShopping from "../assets/icons/cart-shopping.svg";
+import chartSimple from "../assets/icons/chart-simple.svg";
+import house from "../assets/icons/house.svg";
+import lock from "../assets/icons/lock-keyhole.svg";
+import objectsColumn from "../assets/icons/objects-column.svg";
+import user from "../assets/icons/user.svg";
+
+export { cartShopping, chartSimple, house, lock, objectsColumn, user };
