bce5d72a9497d38974e08917156e6bab45536379
Sina Zinsaz <101419921+SINAZZzz@users.noreply.github.com>
2024-07-11 15:50:37 +0330

    feat: update sidebar

--- src/layout/panel/Sidebar.tsx
+++ src/layout/panel/Sidebar.tsx
@@ -17,55 +17,73 @@ import {
   objectsColumn,
   user,
 } from "../../constants/icons";
+import { useState } from "react";
 
 const icons = [house, cartShopping, chartSimple, objectsColumn, user, lock];
-const drawer = (
-  <div>
-    <Toolbar
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        fontSize: "1.7rem",
-        fontWeight: "bold",
-      }}
-    >
-      Sina Zinsz
-    </Toolbar>
-    <List>
-      {[
-        "Dashboard",
-        "NFT Marketplace",
-        "Tables",
-        "Kanban",
-        "Profile",
-        "Sign In",
-      ].map((text, index) => (
-        <ListItem key={text} disablePadding>
-          <ListItemButton sx={{ display: "flex", alignItems: "center" }}>
-            <ListItemIcon sx={{ minWidth: 35 }}>
-              <Box
-                component="img"
-                src={icons[index]}
-                alt={text}
-                sx={{
-                  width: 20,
-                  height: 20,
-                  color: "#A3AED0",
-                  filter:
-                    "invert(53%) sepia(11%) saturate(488%) hue-rotate(181deg) brightness(94%) contrast(92%)",
-                }}
-              />
-            </ListItemIcon>
-            <ListItemText primary={text} sx={{ color: "#A3AED0" }} />
-          </ListItemButton>
-        </ListItem>
-      ))}
-    </List>
-  </div>
-);
 
 const Sidebar = () => {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+
+  const handleListItemClick = (index: number) => {
+    setActiveIndex(index);
+  };
+
+  const drawer = (
+    <div>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "1.7rem",
+          fontWeight: "bold",
+        }}
+      >
+        Sina Zinsz
+      </Toolbar>
+      <List sx={{}}>
+        {[
+          "Dashboard",
+          "NFT Marketplace",
+          "Tables",
+          "Kanban",
+          "Profile",
+          "Sign In",
+        ].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton
+              onClick={() => handleListItemClick(index)}
+              sx={{
+                "&:hover": {
+                  backgroundColor: "inherit",
+                },
+              }}
+            >
+              <ListItemIcon sx={{ minWidth: 35 }}>
+                <Box
+                  component="img"
+                  src={icons[index]}
+                  alt={text}
+                  sx={{
+                    width: 20,
+                    height: 20,
+                    filter:
+                      "invert(53%) sepia(11%) saturate(488%) hue-rotate(181deg) brightness(94%) contrast(92%)",
+                    color: activeIndex === index ? "#fff" : "#A3AED0",
+                  }}
+                />
+              </ListItemIcon>
+              <ListItemText
+                primary={text}
+                sx={{ color: activeIndex === index ? "#000" : "#A3AED0" }}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+
   return (
     <Box
       component="nav"
