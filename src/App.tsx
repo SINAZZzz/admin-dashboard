8c7b4bef55b638cc12bb22dde52802ff9c49d508
Sina Zinsaz <101419921+SINAZZzz@users.noreply.github.com>
2024-07-11 13:29:57 +0330

    feat: add query

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import PanelLayout from "./layout/panel";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
-export default function ResponsiveDrawer() {
-  return <PanelLayout />;
+const queryClient = new QueryClient();
+
+export default function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <PanelLayout />
+      <ReactQueryDevtools
+        initialIsOpen={false}
+        position="left"
+        buttonPosition="bottom-left"
+      />
+    </QueryClientProvider>
+  );
 }
