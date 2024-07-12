0978787cd596f2a11a7d1d55c7aaf9a4b18b934c
Sina Zinsaz <101419921+SINAZZzz@users.noreply.github.com>
2024-07-12 03:32:01 +0330

    feat: add component DrawerSidebar

---
+++ src/components/DrawerSidebar.tsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Toolbar,
+} from "@mui/material";
+import {
+  BarChartRoundedIcon,
+  HomeIcon,
+  LockIcon,
+  PersonIcon,
+  ShoppingCartOutlinedIcon,
+  ViewQuiltRoundedIcon,
+} from "../constants/icons";
+
+export default function DrawerSidebar() {
+  const [activeIndex, setActiveIndex] = useState<number | null>(null);
+  const icons = [
+    HomeIcon,
+    ShoppingCartOutlinedIcon,
+    BarChartRoundedIcon,
+    ViewQuiltRoundedIcon,
+    PersonIcon,
+    LockIcon,
+  ];
+
+  const handleListItemClick = (index: number) => {
+    setActiveIndex(index);
+  };
+  return (
+    <div>
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "1.7rem",
+          fontWeight: "bold",
+        }}
+      >
+        Sina Zinsz
+      </Toolbar>
+      <List sx={{}}>
+        {[
+          "Dashboard",
+          "NFT Marketplace",
+          "Tables",
+          "Kanban",
+          "Profile",
+          "Sign In",
+        ].map((text, index) => (
+          <ListItem key={text} disablePadding>
+            <ListItemButton
+              onClick={() => handleListItemClick(index)}
+              sx={{
+                "&:hover": {
+                  backgroundColor: "inherit",
+                },
+              }}
+            >
+              <ListItemIcon sx={{ minWidth: 35 }}>
+                <Box
+                  component={icons[index]}
+                  sx={{
+                    width: 24,
+                    height: 24,
+                    color: activeIndex === index ? "#4318FF" : "#A3AED0",
+                  }}
+                />
+              </ListItemIcon>
+              <ListItemText
+                primary={text}
+                sx={{ color: activeIndex === index ? "#000" : "#A3AED0" }}
+              />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </div>
+  );
+}
--- src/layout/panel/Sidebar.tsx
+++ src/layout/panel/Sidebar.tsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { drawerWidth } from "../../constants/layout";
 import {
   Box,
@@ -10,83 +9,9 @@ import {
   ListItemText,
   Toolbar,
 } from "@mui/material";
-import {
-  BarChartRoundedIcon,
-  HomeIcon,
-  LockIcon,
-  PersonIcon,
-  ShoppingCartOutlinedIcon,
-  ViewQuiltRoundedIcon,
-} from "../../constants/icons";
-
-const icons = [
-  HomeIcon,
-  ShoppingCartOutlinedIcon,
-  BarChartRoundedIcon,
-  ViewQuiltRoundedIcon,
-  PersonIcon,
-  LockIcon,
-];
+import DrawerSidebar from "../../components/DrawerSidebar";
 
 const Sidebar = () => {
-  const [activeIndex, setActiveIndex] = useState<number | null>(null);
-
-  const handleListItemClick = (index: number) => {
-    setActiveIndex(index);
-  };
-
-  const drawer = (
-    <div>
-      <Toolbar
-        sx={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          fontSize: "1.7rem",
-          fontWeight: "bold",
-        }}
-      >
-        Sina Zinsz
-      </Toolbar>
-      <List sx={{}}>
-        {[
-          "Dashboard",
-          "NFT Marketplace",
-          "Tables",
-          "Kanban",
-          "Profile",
-          "Sign In",
-        ].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton
-              onClick={() => handleListItemClick(index)}
-              sx={{
-                "&:hover": {
-                  backgroundColor: "inherit",
-                },
-              }}
-            >
-              <ListItemIcon sx={{ minWidth: 35 }}>
-                <Box
-                  component={icons[index]}
-                  sx={{
-                    width: 24,
-                    height: 24,
-                    color: activeIndex === index ? "#4318FF" : "#A3AED0",
-                  }}
-                />
-              </ListItemIcon>
-              <ListItemText
-                primary={text}
-                sx={{ color: activeIndex === index ? "#000" : "#A3AED0" }}
-              />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
-    </div>
-  );
-
   return (
     <Box
       component="nav"
@@ -104,7 +29,7 @@ const Sidebar = () => {
         }}
         open
       >
-        {drawer}
+        <DrawerSidebar />
       </Drawer>
     </Box>
   );
