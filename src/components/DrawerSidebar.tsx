9f4d7210247cb2038357662b1b8b10600512885b
SinaZinsaz <sinazinsaz1@gmail.com>
2024-08-09 01:00:00 +0330

    clear sidebar code

--- src/components/DrawerSidebar.tsx
+++ src/components/DrawerSidebar.tsx
@@ -1,98 +1,13 @@
-import { useState } from "react";
-import {
-  Box,
-  Divider,
-  List,
-  ListItem,
-  ListItemButton,
-  ListItemIcon,
-  ListItemText,
-} from "@mui/material";
-import { Link as RouterLink } from "react-router-dom";
-import { links } from "../constants/links";
-import { icons } from "../constants/icons";
-import { useSelector } from "react-redux";
-import { RootState } from "../store";
-import Title from "./header/Title";
+import { Divider } from "@mui/material";
+import Title from "./sidebar/Title";
+import ListSidebar from "./sidebar/list";
 
 export default function DrawerSidebar() {
-  const [activeIndex, setActiveIndex] = useState<number | null>();
-  const isDarkMode = useSelector((state: RootState) => state.theme.darkMode);
-
-  const handleListItemClick = (index: number) => {
-    setActiveIndex(index);
-  };
-
   return (
     <div>
-      {/* title */}
       <Title />
       <Divider sx={{ mb: "1rem" }} />
-      <List>
-        {[
-          "Dashboard",
-          "NFT Marketplace",
-          "Tables",
-          "Kanban",
-          "Profile",
-          "Sign In",
-        ].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton
-              component={RouterLink}
-              to={links[index]}
-              onClick={() => handleListItemClick(index)}
-              sx={{
-                "&:hover": {
-                  backgroundColor: "inherit",
-                },
-                position: "relative",
-                "&::after": {
-                  content: '""',
-                  position: "absolute",
-                  right: 0,
-                  top: 5,
-                  bottom: 0,
-                  width: 4,
-                  height: "calc(100% - 10px)",
-                  borderRadius: "5px",
-                  backgroundColor:
-                    activeIndex === index ? "#4318FF" : "transparent",
-                },
-              }}
-            >
-              <ListItemIcon sx={{ minWidth: 35 }}>
-                <Box
-                  component={icons[index]}
-                  sx={{
-                    width: 24,
-                    height: 24,
-                    color: !isDarkMode
-                      ? activeIndex === index
-                        ? "#4318FF"
-                        : "#A3AED0"
-                      : activeIndex === index
-                      ? "#fff"
-                      : "#A3AED0",
-                  }}
-                />
-              </ListItemIcon>
-              <ListItemText
-                primary={text}
-                sx={{
-                  color: !isDarkMode
-                    ? activeIndex === index
-                      ? "#000"
-                      : "#A3AED0"
-                    : activeIndex === index
-                    ? "#fff"
-                    : "#A3AED0",
-                }}
-              />
-            </ListItemButton>
-          </ListItem>
-        ))}
-      </List>
+      <ListSidebar />
     </div>
   );
 }
---
+++ src/components/sidebar/list/index.tsx
@@ -0,0 +1,91 @@
+import {
+  Box,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+import { links } from "../../../constants/links";
+import { icons } from "../../../constants/icons";
+import { useSelector } from "react-redux";
+import { useState } from "react";
+import { RootState } from "../../../store";
+
+export default function ListSidebar() {
+  const [activeIndex, setActiveIndex] = useState<number | null>();
+  const isDarkMode = useSelector((state: RootState) => state.theme.darkMode);
+
+  const handleListItemClick = (index: number) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <List>
+      {[
+        "Dashboard",
+        "NFT Marketplace",
+        "Tables",
+        "Kanban",
+        "Profile",
+        "Sign In",
+      ].map((text, index) => (
+        <ListItem key={text} disablePadding>
+          <ListItemButton
+            component={RouterLink}
+            to={links[index]}
+            onClick={() => handleListItemClick(index)}
+            sx={{
+              "&:hover": {
+                backgroundColor: "inherit",
+              },
+              position: "relative",
+              "&::after": {
+                content: '""',
+                position: "absolute",
+                right: 0,
+                top: 5,
+                bottom: 0,
+                width: 4,
+                height: "calc(100% - 10px)",
+                borderRadius: "5px",
+                backgroundColor:
+                  activeIndex === index ? "#4318FF" : "transparent",
+              },
+            }}
+          >
+            <ListItemIcon sx={{ minWidth: 35 }}>
+              <Box
+                component={icons[index]}
+                sx={{
+                  width: 24,
+                  height: 24,
+                  color: !isDarkMode
+                    ? activeIndex === index
+                      ? "#4318FF"
+                      : "#A3AED0"
+                    : activeIndex === index
+                    ? "#fff"
+                    : "#A3AED0",
+                }}
+              />
+            </ListItemIcon>
+            <ListItemText
+              primary={text}
+              sx={{
+                color: !isDarkMode
+                  ? activeIndex === index
+                    ? "#000"
+                    : "#A3AED0"
+                  : activeIndex === index
+                  ? "#fff"
+                  : "#A3AED0",
+              }}
+            />
+          </ListItemButton>
+        </ListItem>
+      ))}
+    </List>
+  );
+}
