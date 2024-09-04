ae103a91a45d0177be3adfc7a44543187f804fdc
SinaZinsaz <sinazinsaz1@gmail.com>
2024-09-05 01:25:15 +0330

    feat: create loading

--- src/components/loading/index.tsx
+++ src/components/loading/index.tsx
@@ -1,6 +1,24 @@
 import { Box } from "@mui/material";
-
+import "./style.css";
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
 
 export default function Loading() {
-  return <Box>Loading...</Box>;
+  const isDarkMode = useSelector((state: RootState) => state.theme.darkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.style.background = "#0B1437"; // Dark mode background
+    } else {
+      document.body.style.background = "#F4F7FE"; // Light mode background
+    }
+  }, [isDarkMode]);
+
+  return (
+    <Box className="spinner">
+      <Box className="loading"></Box>
+      <Box className="loading-text">Loading...</Box>
+    </Box>
+  );
 }
